488666c8d7c771d57e298e2a120cc7357579f76b
RaitonOwO <jhoanowo@gmail.com>
2024-06-28 14:41:17 -0500

    feat: :sparkles: se cambio el menu de validar al usuario a menu.py

--- main.py
+++ main.py
@@ -4,24 +4,9 @@ from menus import*
 RUTA_JSON = "usuarios.json"
 datosUsuario = bajar_datos(RUTA_JSON)
 
-def validar_inicio_sesion(datosUsuario):
-    id_usuario=input("Ingrese su numero de cedula: ")
-    contrasena_usuario=input("Ingrese su contraseña: ")
-    
-    for usuario in datosUsuario["usuarios"]:
-        if usuario["id"]==id_usuario and usuario["contrasena"]==contrasena_usuario:
-            print("╭───────────────────────────────────────╮")
-            print("│                                       │")
-            print("│        ¡Bienvenido a MainTech!        │")
-            print("│                                       │")
-            print("╰───────────────────────────────────────╯")
-            return True
-    
-    print("╭───────────────────────────────────────╮")
-    print("│       ¡Credenciales incorrectas!      │")
-    print("│  Intentelo de nuevo o lo invitamos    │")
-    print("│  a registrarse en nuestro aplicativo  │")
-    print("╰───────────────────────────────────────╯")
-    return False
+while True:
+    if validar_inicio_sesion(datosUsuario):
+        break
 
-validar_inicio_sesion(datosUsuario)
+
+
--- menus.py
+++ menus.py
@@ -16,3 +16,22 @@ def pedir_opcion():
         print("╰───────────────────────────────────────╯")
         return -1
 
+def validar_inicio_sesion(datosUsuario):
+    id_usuario=input("Ingrese su numero de cedula: ")
+    contrasena_usuario=input("Ingrese su contraseña: ")
+    
+    for usuario in datosUsuario["usuarios"]:
+        if usuario["id"]==id_usuario and usuario["contrasena"]==contrasena_usuario:
+            print("╭───────────────────────────────────────╮")
+            print("│                                       │")
+            print("│        ¡Bienvenido a MainTech!        │")
+            print("│                                       │")
+            print("╰───────────────────────────────────────╯")
+            return True
+    
+    print("╭───────────────────────────────────────╮")
+    print("│       ¡Credenciales incorrectas!      │")
+    print("│  Intentelo de nuevo o lo invitamos    │")
+    print("│  a registrarse en nuestro aplicativo  │")
+    print("╰───────────────────────────────────────╯")
+    return False
