5987f54a6e3ee1a817651663e95e963223b80d43
RaitonOwO <jhoanowo@gmail.com>
2024-06-28 16:38:48 -0500

    feat: :sparkles: Json Servicios
    Se modifico la estructura Json de Servicios

---
+++ modulo_servicios.py
@@ -0,0 +1,10 @@
+def registro_mantenimiento():
+    while True:
+        print("ingrese el numero del servicio que desea contratar | 1.limpieza, 2. Reestblecer Equipo, 3.comprar e instalar productos")
+        servicio = int(input(""))
+    
+    
+    
+    
+    
+registro_mantenimiento()
