830e2261bd2adf80ecf8ec48908e27b6f9aa983d
JuanJTorresB <juanjtorresbecerra@gmail.com>
2024-06-28 10:48:17 -0500

    feat: :sparkles: Inicio de Sesion
    Se creo la funcion que valida el inicio de sesion

--- main.py
+++ main.py
@@ -2,9 +2,26 @@ from datos import *
 from modulo_tecnicos import*
 from menus import*
 RUTA_JSON = "usuarios.json"
-datos = bajar_datos(RUTA_JSON)
+datosUsuario = bajar_datos(RUTA_JSON)
 
+def validar_inicio_sesion(datosUsuario):
+    id_usuario=input("Ingrese su numero de cedula: ")
+    contrasena_usuario=input("Ingrese su contraseña: ")
+    
+    for usuario in datosUsuario["usuarios"]:
+        if usuario["id"]==id_usuario and usuario["contrasena"]==contrasena_usuario:
+            print("╭───────────────────────────────────────╮")
+            print("│                                       │")
+            print("│        ¡Bienvenido a MainTech!        │")
+            print("│                                       │")
+            print("╰───────────────────────────────────────╯")
+            return True
+    
+    print("╭───────────────────────────────────────╮")
+    print("│       ¡Credenciales incorrectas!      │")
+    print("│  Intentelo de nuevo o lo invitamos    │")
+    print("│  a registrarse en nuestro aplicativo  │")
+    print("╰───────────────────────────────────────╯")
+    return False
 
-
-
-
+validar_inicio_sesion(datosUsuario)
