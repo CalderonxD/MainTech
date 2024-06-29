6fe8aa7ebd9adf37a8e2c26f781a0c727bd6c2ab
RaitonOwO <jhoanowo@gmail.com>
2024-06-29 18:59:26 -0500

    feat: :sparkles: python cache

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
+#imports
 from datos import *
 from datos import bajar_datos
 from modulo_usuarios import *
 from modulo_tecnicos import *
 from menus import *
+from modulo_servicios import *
 from modulo_servicios import registro_mantenimiento
+from datos import subir_datos
+#constantes
+RUTA_USUARIOS = "usuarios.json"
+RUTA_SERVICIOS = "servicios.json"
+#cargar datos
+while True:
+    datosUsuario = bajar_datos(RUTA_USUARIOS)
+    datosServicios = bajar_datos(RUTA_SERVICIOS)
+    #datosUsuario = eliminar_usuario(datosUsuario)
+    
+    #datosUsuario = editar_usuario(datosUsuario)
+    
+    # agregar_servicio(datosServicios)
+    #registro_mantenimiento(datosUsuario,datosServicios)
+    #subir_datos(datosServicios,RUTA_SERVICIOS)
 
-RUTA_JSON = "usuarios.json"
-datosUsuario = bajar_datos(RUTA_JSON)
+    #(datosServicios)
+    #subir_datos(datosServicios,RUTA_SERVICIOS)       
+    #break
 
-datosUsuario = eliminar_usuario(datosUsuario)
-
-datosUsuario = editar_usuario(datosUsuario)
-
-""" while True:
-    if validar_inicio_sesion(datosUsuario):
-        break
-
-registro_mantenimiento(datosUsuario)
- """
+    mostrar_servicios(datosServicios)
+    break
