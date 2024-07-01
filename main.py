93c7fdd0d9537b70922e937c8f5e8812fb8a3b6d
RaitonOwO <jhoanowo@gmail.com>
2024-07-01 18:39:12 -0500

    feat: :sparkles: se empezo la creacion del main para ejecutar el programa

--- main.py
+++ main.py
@@ -5,35 +5,42 @@ from modulo_usuarios import *
 from modulo_tecnicos import *
 from menus import *
 from modulo_servicios import *
-from datos import subir_datos
 from contactanos import*
 from preguntasyrespuesta import*
 from modulo_ventas import *
+from modulo_pago import*
+from menus import ejecutable
+
 #constantes
 RUTA_USUARIOS = "usuarios.json"
 RUTA_SERVICIOS = "servicios.json"
 RUTA_HISTORIAL_VENTAS = "historial_ventas.json"
+
+
 #cargar datos
 while True:
     datosUsuario = bajar_datos(RUTA_USUARIOS)
     datosServicios = bajar_datos(RUTA_SERVICIOS)
-    #datosUsuario = eliminar_usuario(datosUsuario)
-    
-    #datosUsuario = editar_usuario(datosUsuario)
-    
-    # agregar_servicio(datosServicios)
-    registro_mantenimiento(datosUsuario,datosServicios)
-    #subir_datos(datosServicios,RUTA_SERVICIOS)
+    #opc = pedir_opcion()
+    #mostrar_menu_principal()
+    #pedir_opcion()
+    ejecutable(datosUsuario)
+   # if opc == 1:
+    #recargar_billetera("1097097808",datosUsuario)
+            
+    #elif opc == 2:
+        #print("Opción para registrar servicio seleccionada.")
+            
+    #elif opc == 3:
+        #print("Opción para registrar compra seleccionada.")
+           
+    #elif opc == 4:
+        #print("Opción para mostrar historial de ventas seleccionada.")
+            
+    #elif opc == 5:
+        #print("Saliendo del programa. ¡Hasta luego!")
+        #break
+    #else:
+        #print("Opción no válida. Por favor, intente de nuevo.")
 
-    #(datosServicios)
-    #subir_datos(datosServicios,RUTA_SERVICIOS)       
-    #break
-
-    #mostrar_servicios(datosServicios)
-    #break
-    
-    #empresa.mostrar_informacion_contacto()
-    #mostrar_informacion_contacto(empresa)
-    #mostrar_ventas_ganancias(RUTA_HISTORIAL_VENTAS)
-    break
 
